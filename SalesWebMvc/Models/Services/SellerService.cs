b9838c1466eb6e1e81149cba6ed4a732b21c9c2d
Roberta Okada <robertaokada144@gmail.com>
2024-07-16 11:08:42 -0300

    seller details e eager loading para mostrar os details vom departamento

--- SalesWebMvc/Models/Services/SellerService.cs
+++ SalesWebMvc/Models/Services/SellerService.cs
@@ -1,5 +1,5 @@
 ﻿using SalesWebMvc.Data;
-
+using Microsoft.EntityFrameworkCore;
 namespace SalesWebMvc.Models.Services
 {
     public class SellerService
@@ -24,7 +24,7 @@ namespace SalesWebMvc.Models.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj =>obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
@@ -38,5 +38,7 @@ namespace SalesWebMvc.Models.Services
             _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
+
+       
     }
 }
